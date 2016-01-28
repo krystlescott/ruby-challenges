c60e8e655bf15c94a0a6a9fbddf813ba92ba13db
Krystle Scott <krystlescott12@gmail.com>
2016-01-28 17:13:26 -0600

    Added Skillcrush code to blog

--- blog.rb
+++ blog.rb
@@ -1,3 +1,4 @@
+=begin
 class Blog
 
   @@total_blogposts = 0
@@ -5,49 +6,128 @@ class Blog
 
   def initialize
     @@total_blogposts += 1
+
+    @@all_blogposts.each do |post|
+      @@all_blogposts.push(BlogPost)
+    end
+  end
+
+  def publish
+    @@all_blogposts.each do |post|
+      puts "Title: #{title}"
+      puts "Author: #{author}"
+      puts "Content: #{content}"
+      puts "Publish Date: #{publish_date}"
+    end
   end
 end
 
 class BlogPost < Blog
 
-  puts "Do you want to create another blog post? [Y/N]"
-  answer = gets
-  if (answer == "Y")
+  attr_accessor :title, :content, :publish_date, :author
 
-
-  def set_title=(title)
+  def initialize
     @title = title
-  end
-
-  def get_title
-    return @title
-  end
-
-  def set_content=(content)
     @content = content
-  end
-
-  def get_content
-    return @content
-  end
-
-  def set_publish_date=(publish_date)
     @publish_date = publish_date
-  end
-
-  def get_publish_date
-    return @publish_date
-  end
-
-  def set_author=(author)
     @author = author
   end
 
-  def get_author
-    return @author
+  def save
+    @@all_blogposts.push(BlogPost)
   end
 
-  def publish
-    puts
+  def new_post(title, content, publish_date, author)
+    post = BlogPost.new
+    puts "What is the title of your blog post?"
+    post.title = gets.chomp
+    puts "What is your name?"
+    post.author = gets.chomp
+    puts "Write the contents of your post here:"
+    post.content = gets.chomp
+    puts "Enter today's date:"
+    post.publish_date = gets.chomp
+    post.save
+    puts "Do you want to create another blog post? [Y/N]"
+    if gets.chomp.downcase == 'y' then new_post(title, content, publish_date, author)
   end
+
+
 end
+
+BlogPost.new.new_post("My First Blog", "Yaay! I finally got this to work!", "01/26/2016", "Krystle Scott") end
+=end
+class Blog
+
+  @@all_blog_posts = []
+  @@num_blog_posts = 0
+
+  def self.all
+    @@all_blog_posts
+  end
+
+  def self.add(thing)
+    @@all_blog_posts << thing
+    @@num_blog_posts += 1
+  end
+
+  def self.publish
+    @@all_blog_posts.each do |post|
+      puts "Title:\n #{post.title}"
+      puts "Body:\n #{post.content}"
+      puts "Publish Date:\n #{post.created_at}"
+    end
+  end
+
+end
+
+
+class BlogPost < Blog
+
+  def self.create
+    post = new
+    puts "Name your blog post:"
+    post.title = gets.chomp
+    puts "Your blog post content:"
+    post.content = gets.chomp
+    post.created_at = Time.now
+    post.save
+    puts "Do you want to create another post? [Y/N]"
+    create if gets.chomp.downcase == 'y'
+ #returns if not ‘y’ and stops the rest of the script from running. otherwise, continues to run ‘create’ method.
+  end
+
+  def title
+    @title
+  end
+
+  def title=(title) # a setter method always takes an argument
+    @title = title # don't forget to set the instance variable
+  end
+
+  def created_at
+    @created_at
+  end
+
+  def created_at=(created_at)
+    @created_at = created_at
+  end
+
+  def content
+    @content
+  end
+
+  def content=(content)
+    @content = content
+  end
+
+  def save
+    BlogPost.add(self)
+  end
+
+end
+
+BlogPost.create
+all_blog_posts = BlogPost.all
+puts all_blog_posts.inspect
+BlogPost.publish
